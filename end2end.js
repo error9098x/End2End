e9f023e1218af384afacb9743d7ec2cd48272d9c
error9098x <43810146+error9098x@users.noreply.github.com>
2022-10-06 09:40:14 +0530

    Add files via upload

---
+++ end2end.js
@@ -0,0 +1,93 @@
+const crypto   = require('crypto');
+const readlineSync = require('readline-sync');
+
+var key, index;
+var textToEncrypt, textToDecrypt;
+var encrypted, decrypted;
+var options, algo;
+var cipher, decipher;
+
+initializeArrays();
+displayWelcomeText();
+
+var choice = startProgram();
+if (options[choice] == 'Send encrypted message') {
+    key = encryptionKeyQuestion();
+    index = encryptionAlgorithmQuestion(); 
+    if (index == -1) {
+        return true;
+    }
+    cipher = createEncryptionCipher(index, key);
+    textToEncrypt = encryptTextQuestion();
+    encrypted = cipher.update(textToEncrypt, 'utf8', 'hex');     
+    encrypted += cipher.final('hex');
+    displayEncryptedText();
+} else if (options[choice] == 'Decrypt your message') {
+    key = decryptionKeyQuestion();
+    index = decryptionAlgorithmQuestion();
+    if (index == -1) {
+        return true;
+    }
+    decipher = createDecryptionCipher(index, key);
+    textToDecrypt = decryptTextQuestion();
+    decrypted = decipher.update(textToDecrypt, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    displayDecryptedText();
+} else {
+    return true;    
+}
+
+function initializeArrays() {
+    options = ['Send encrypted message', 'Decrypt your message'];
+    algo = [ "aes128", "aes192", "aes256", "des3", "desx", "rc2"];
+}
+
+function displayWelcomeText() {
+    console.log("Welcome to Encrypted Chats!")
+}
+
+function startProgram() {
+    return readlineSync.keyInSelect(options, 'What would you like to do?');
+}
+
+function displayDecryptedText() {
+    console.log("Decrypted Message = " + decrypted);
+}
+
+function decryptTextQuestion() {
+    return readlineSync.question("Enter text to decrypt: ");
+}
+
+function createDecryptionCipher(index, key) {
+    return crypto.createDecipher(algo[index], key);
+}
+
+function decryptionAlgorithmQuestion() {
+    return readlineSync.keyInSelect(algo, 'Select an algorithm for decryption');
+}
+
+function decryptionKeyQuestion() {
+    return readlineSync.question('Enter a key for the decryption cipher: ');
+}
+
+function encryptionKeyQuestion() {
+    return readlineSync.question('Enter a key for the encryption cipher: ');
+}
+
+function encryptionAlgorithmQuestion() {
+    return readlineSync.keyInSelect(algo, 'Select an encryption algorithm');
+}
+
+function createEncryptionCipher(index, key) {
+    return crypto.createCipher(algo[index], key);
+}
+
+function encryptTextQuestion() { 
+    return readlineSync.question('Enter text to encrypt: ', {
+        hideEchoBack: true    
+    });
+}
+
+function displayEncryptedText() {
+    console.log("Encrypted Message = " + encrypted);
+}
